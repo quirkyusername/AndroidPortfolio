a9c6a6e77d4f2c242db0c847c7b7c09412497753
Ahmed <ahmed_abdalla@outlook.com>
2016-03-20 23:07:45 -0400

    Clean up. Removed wear module.

--- mobile/src/main/java/com/rubberapps/ahmed/myappportfolio/MainActivity.java
+++ mobile/src/main/java/com/rubberapps/ahmed/myappportfolio/MainActivity.java
@@ -1,9 +1,6 @@
 package com.rubberapps.ahmed.myappportfolio;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,10 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
